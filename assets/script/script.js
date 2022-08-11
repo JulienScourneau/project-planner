b22107726c6e15d8dd9b9059e67a142706a9615f
Julien Scourneau <scourneaujulien@outlook.com>
2022-08-11 13:56:51 +0200

    Added event to display or hide Add task area

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,15 @@
 document.querySelector("#new-date").valueAsDate = new Date();
+let div = document.getElementById("add_task_container");
+
+
+/**
+ * Display or hide add task area
+ */
+document.getElementById("add_task_button").addEventListener("click", () => {
+
+    if (window.getComputedStyle(div, null).getPropertyValue("display") == "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+});
